9f730b5d5595ea74105f6d5defc9e58a1a18d84c
HopeLynx <grigoriy.sizov@gmail.com>
2021-03-31 13:42:17 +0300

    Update 1389.py
    added second variant

--- 1389.py
+++ 1389.py
@@ -8,3 +8,12 @@ class Solution:
 # Runtime: 32 ms, faster than 77.06% of Python3 online submissions for Create Target Array in the Given Order.
 # Memory Usage: 14.3 MB, less than 49.25% of Python3 online submissions for Create Target Array in the Given Order.
 
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            ans.insert(index.pop(0),nums.pop(0))
+        return ans
+    
+# Runtime: 28 ms, faster than 92.25% of Python3 online submissions for Create Target Array in the Given Order.
+# Memory Usage: 14.3 MB, less than 49.25% of Python3 online submissions for Create Target Array in the Given Order.

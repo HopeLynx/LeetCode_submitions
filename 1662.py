90646d35957e9c570c24c65e03dbce4a1bc69926
HopeLynx <grigoriy.sizov@gmail.com>
2021-03-31 18:39:49 +0300

    Create 1662.py

---
+++ 1662.py
@@ -0,0 +1,6 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return ''.join(word1) == ''.join(word2)
+      
+# Runtime: 24 ms, faster than 95.91% of Python3 online submissions for Check If Two String Arrays are Equivalent.
+# Memory Usage: 14.3 MB, less than 34.44% of Python3 online submissions for Check If Two String Arrays are Equivalent.

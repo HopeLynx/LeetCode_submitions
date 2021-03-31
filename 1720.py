cd78eae5f666577eb0ace07166f19b82aa21a4a9
HopeLynx <grigoriy.sizov@gmail.com>
2021-03-31 13:27:56 +0300

    Update 1720.py
    added second variant

--- 1720.py
+++ 1720.py
@@ -8,3 +8,15 @@ class Solution:
       
 # Runtime: 288 ms, faster than 6.67% of Python3 online submissions for Decode XORed Array.
 # Memory Usage: 15.5 MB, less than 99.66% of Python3 online submissions for Decode XORed Array.
+
+
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        ans = [first]
+#         xor(a,b) = a^b
+        for i in range(len(encoded)):
+            ans.append(ans[i]^encoded[i])
+        return ans
+
+# Runtime: 224 ms, faster than 77.38% of Python3 online submissions for Decode XORed Array.
+# Memory Usage: 16 MB, less than 17.57% of Python3 online submissions for Decode XORed Array.

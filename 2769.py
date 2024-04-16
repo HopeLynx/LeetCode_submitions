8c48f36046066e2abdcb43f845eac17186745200
Grigorii <hopelynxstudio@gmail.com>
2024-04-16 18:53:38 +0800

    added solution for problem 2769

--- 2769.py
+++ 2769.py
@@ -1,6 +1,6 @@
 class Solution:
     def theMaximumAchievableX(self, num: int, t: int) -> int:
-        return num + t * 2
-
+        return num + t + t
+# *2 works slower
 # Runtime: 35 ms Beats 79.91% of users with Python3
-# Memory Usage: 16.50 MB Beats 39.54% of users with Python3
+# Memory Usage: 16.47 MB Beats 91.71% of users with Python3

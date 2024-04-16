85b66f97d06ea4f6f30744dc2e0bb63ca3beede3
Grigorii <hopelynxstudio@gmail.com>
2024-04-16 14:58:37 +0800

    added solution for problem 1929

---
+++ 1929.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # return [*nums,*nums]
+        return nums + nums
+
+# Runtime: 70 ms, faster than 57.84% of Python3 online submissions.
+# Memory Usage: 16.88 MB, less than 73.53% of Python3 online submissions.

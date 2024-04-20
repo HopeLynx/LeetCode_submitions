b7bb675754ee978757ff7232871fd5ac96a98ba7
Grigorii <hopelynxstudio@gmail.com>
2024-04-20 20:43:23 +0800

    added solution for problem 1637

---
+++ 1637.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        real_points = []
+        for i in range(0, len(points)):
+            real_points.append(points[i][0])
+        real_points.sort()
+        max = 0
+        for i in range(1, len(points)):
+            if max < real_points[i] - real_points[i - 1]:
+                max = real_points[i] - real_points[i - 1]
+        return max
+
+# Runtime: 677 ms Beats 73.82% of users with Python3
+# Memory Usage: 58.98 MB Beats 74.22% of users with Python3

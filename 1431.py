2d8421238d5599a2f5bac3532a05dd17b467253d
Grigorii <hopelynxstudio@gmail.com>
2024-04-21 16:42:04 +0800

    added solution for problem 1431

--- 1431.py
+++ 1431.py
@@ -1,12 +1,12 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         max_c = max(candies)
-        x = list()
+        x = []
         for i in range(0,len(candies)):
             if (max_c <= candies[i]+extraCandies):
                 x.append(True)
             else: 
                 x.append(False)
         return x
-#Runtime: 40 ms, faster than 57.65% of Python3 online submissions for Kids With the Greatest Number of Candies.
-#Memory Usage: 14.3 MB, less than 56.68% of Python3 online submissions for Kids With the Greatest Number of Candies.
+#Runtime: 34 ms, faster than 90.49% of Python3 online submissions for Kids With the Greatest Number of Candies.
+#Memory Usage: 16.48 MB, less than 90.98% of Python3 online submissions for Kids With the Greatest Number of Candies.
